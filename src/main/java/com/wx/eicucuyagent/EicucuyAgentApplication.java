9ced19b25afb852a316ecc524639fbf0a09e332d
wangxiang <xiangwang04@hand-china.com>
2022-11-27 17:12:52 +0800

    [FIX] fix

--- src/main/java/com/wx/eicucuyagent/EicucuyAgentApplication.java
+++ src/main/java/com/wx/eicucuyagent/EicucuyAgentApplication.java
@@ -10,6 +10,7 @@ public class EicucuyAgentApplication {
         // Java 操作K8S的API
         // https://blog.csdn.net/a772304419/article/details/126725026
         // https://www.kubernetes.org.cn/8984.html
+        // https://huaweicloud.csdn.net/6331148bd3efff3090b51964.html
         SpringApplication.run(EicucuyAgentApplication.class, args);
     }
 
