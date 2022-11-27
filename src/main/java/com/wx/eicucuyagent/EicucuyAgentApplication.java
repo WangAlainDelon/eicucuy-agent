90b56dd2f15dd1e32724552ce1df7bab77d711e3
wangxiang <xiangwang04@hand-china.com>
2022-11-27 14:41:29 +0800

    [ADD] init

---
+++ src/main/java/com/wx/eicucuyagent/EicucuyAgentApplication.java
@@ -0,0 +1,16 @@
+package com.wx.eicucuyagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EicucuyAgentApplication {
+
+    public static void main(String[] args) {
+        // Java 操作K8S的API
+        // https://blog.csdn.net/a772304419/article/details/126725026
+        // https://www.kubernetes.org.cn/8984.html
+        SpringApplication.run(EicucuyAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wx/eicucuyagent/EicucuyAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wx.eicucuyagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EicucuyAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
